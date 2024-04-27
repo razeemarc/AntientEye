2f749b0dd313f116f35b9f039ce709959e78d6b3
razeemarc <razeemarc@gmail.com>
2024-04-27 21:02:40 +0530

    Firebase Integrated

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_text_to_speech_tutorial/pages/welcome_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options:FirebaseOptions(
+          apiKey: "AIzaSyBMRaDxuNKEUzPVTnNhXSkhrCn2iPOw14s",
+          appId: "1:268201396807:android:e1a72471a67e30d2ae3f62",
+          messagingSenderId:"268201396807",
+          projectId: "adminpanel-e1d50"
+      )
+      );
   runApp(const MyApp());
 }
 
